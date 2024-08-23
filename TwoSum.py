e1ac35a03388579de2dd525ea1e08f0fb1e5b37b
codewithash <134801667+codewithash-hub@users.noreply.github.com>
2024-08-23 21:44:54 +0200

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -6,4 +6,15 @@ for i in range(len(nums)):
         if nums[i] + nums[j] == target:
             print([i, j])
 
-        
+# time complexity: O(n^2)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+
+        for i, num in enumerate(nums):
+            if target - num in my_dict:
+                return [i, my_dict[target - num]]
+            my_dict[num] = i
+
+# time cmplexity: O(n) - Linear time
